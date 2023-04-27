c2a5f47acad2e8f6dcb3959239f439a082cbb576
rasm445f <90387951+rasm445f@users.noreply.github.com>
2023-04-27 11:40:16 +0200

    missed some small refactors

--- api/src/resolvers/mutation.ts
+++ api/src/resolvers/mutation.ts
@@ -1,38 +1,38 @@
 // import { movies, ratings } from '../data';
 import { Movie, Context, Args } from '../types';
-  export default {
-    createMovie: (_parent:Movie, { input }:Args, {movies}:Context) => {
-      if('director' in input){ // input is a Movie
-        const newMovie: Movie = {
-          id: String(movies.length + 1),
-          title: input.title,
-          director: input.director,
-        };
-        console.log('input: ', input, newMovie);
-        movies.push(newMovie);
-        return newMovie;
-      } else {
-        return null;
-      }
-      
-    },
 
-    deleteMovie: (_parent:never, { id }:Args, {movies}:Context) => {
-      const index = movies.findIndex(person => person.id === id);
-      if (index === -1) {
-        return false; // person not found
-      }
-      movies.splice(index, 1);
-      return true; // deletion successful
-    },
-    updateMovie: (_parent: never, { id, input }:Args, {movies}:Context) => {
-      const index = movies.findIndex(person => person.id === id);
-      if (index === -1) {
-        return null; // person not found
-      }
-      const movie = movies[index];
-      const updatedMovie = { ...movie, ...input };
-      movies[index] = updatedMovie;
-      return updatedMovie;
+export default {
+  createMovie: (_parent: undefined, { input }: Args, { movies }: Context) => {
+    if ('director' in input) {
+      const newMovie: Movie = {
+        id: String(movies.length + 1),
+        title: input.title,
+        director: input.director,
+      };
+      movies.push(newMovie);
+      return newMovie;
+    } else {
+      return null;
     }
-  }
+  },
+
+  deleteMovie: (_parent: never, { id }: Args, { movies }: Context) => {
+    const index = movies.findIndex(movie => movie.id === id);
+    if (index === -1) {
+      return false;
+    }
+    movies.splice(index, 1);
+    return true;
+  },
+
+  updateMovie: (_parent: never, { id, input }: Args, { movies }: Context) => {
+    const index = movies.findIndex(movie => movie.id === id);
+    if (index === -1) {
+      throw new Error('Movie not found');
+    }
+    const movie = movies[index];
+    const updatedMovie = { ...movie, ...input };
+    movies[index] = updatedMovie;
+    return updatedMovie;
+  },
+};
