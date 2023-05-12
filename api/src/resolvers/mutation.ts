d29948647e8ebee5931b84057e2251530efdf963
rasm445f <90387951+rasm445f@users.noreply.github.com>
2023-05-12 09:56:47 +0200

    changed the updateMovie function in mutation.ts in the api

--- api/src/resolvers/mutation.ts
+++ api/src/resolvers/mutation.ts
@@ -19,6 +19,7 @@ export default {
     return true;
   },
 
+  /*
   updateMovie: (_parent: never, { id, input }: Args, { movies }: Context) => {
     const index = movies.findIndex(movie => movie.id === id);
     if (index === -1) {
@@ -29,4 +30,24 @@ export default {
     movies[index] = updatedMovie;
     return updatedMovie;
   },
+
+    */
+
+
+
+  updateMovie: async (_parent: never, { id, input }: Args, _context: Context) => {
+    try {
+      const updatedMovie = await movieModel.findByIdAndUpdate(id, input, { new: true });
+
+      if (!updatedMovie) {
+        throw new Error('Movie not found');
+      }
+
+      return updatedMovie;
+    } catch (error) {
+      throw new Error('Failed to update movie');
+    }
+  },
+
+
 };
